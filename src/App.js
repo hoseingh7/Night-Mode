b65ee98d588526498e63d1dc9908bcddb78dbe8b
hoseingh7 <hoseingholamii1376@gmail.com>
2023-10-26 06:41:49 -0700

    toggel ion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,381 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [date, setDate] = useState("");
+  const [theme, setTheme] = useState("dark");
+  const [sun, setSun] = useState("");
+  const [moon, setMoon] = useState("hide");
+  let a;
+  let time;
+
+  let handleClick = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+    theme === "dark"
+      ? () => {
+          setMoon("hide");
+          setSun("");
+        }
+      : () => {
+          setSun("hide");
+          setMoon("");
+        };
+  };
+  let handleWatch = () => {
+    a = new Date();
+    time = a.getHours() + ":" + a.getMinutes();
+    setDate(time);
+  };
+
+  setInterval(() => {
+    handleWatch();
+  }, 1000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <div className={`container ${theme} `}>
+        <span
+          className="btn"
+          onClick={handleClick}>
+          <svg
+            className={moon}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24">
+            <g
+              fill="#fff"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2">
+              <g stroke-dasharray="2">
+                <path d="M12 21v1M21 12h1M12 3v-1M3 12h-1">
+                  <animate
+                    fill="freeze"
+                    attributeName="stroke-dashoffset"
+                    dur="0.2s"
+                    values="4;2"
+                  />
+                </path>
+                <path d="M18.5 18.5l0.5 0.5M18.5 5.5l0.5 -0.5M5.5 5.5l-0.5 -0.5M5.5 18.5l-0.5 0.5">
+                  <animate
+                    fill="freeze"
+                    attributeName="stroke-dashoffset"
+                    begin="0.2s"
+                    dur="0.2s"
+                    values="4;2"
+                  />
+                </path>
+              </g>
+              <path
+                d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z"
+                opacity="0">
+                <set
+                  attributeName="opacity"
+                  begin="0.5s"
+                  to="1"
+                />
+              </path>
+            </g>
+            <g
+              fill="#fff"
+              fill-opacity="0">
+              <path d="M15.22 6.03L17.75 4.09L14.56 4L13.5 1L12.44 4L9.25 4.09L11.78 6.03L10.87 9.09L13.5 7.28L16.13 9.09L15.22 6.03Z">
+                <animate
+                  fill="freeze"
+                  attributeName="fill-opacity"
+                  begin="0.6s"
+                  dur="0.4s"
+                  values="0;1"
+                />
+              </path>
+              <path d="M19.61 12.25L21.25 11L19.19 10.95L18.5 9L17.81 10.95L15.75 11L17.39 12.25L16.8 14.23L18.5 13.06L20.2 14.23L19.61 12.25Z">
+                <animate
+                  fill="freeze"
+                  attributeName="fill-opacity"
+                  begin="1s"
+                  dur="0.4s"
+                  values="0;1"
+                />
+              </path>
+            </g>
+            <mask id="lineMdSunnyOutlineToMoonTransition0">
+              <circle
+                cx="12"
+                cy="12"
+                r="12"
+                fill="#fff"
+              />
+              <circle
+                cx="12"
+                cy="12"
+                r="4">
+                <animate
+                  fill="freeze"
+                  attributeName="r"
+                  begin="0.1s"
+                  dur="0.4s"
+                  values="4;8"
+                />
+              </circle>
+              <circle
+                cx="22"
+                cy="2"
+                r="3"
+                fill="#fff">
+                <animate
+                  fill="freeze"
+                  attributeName="cx"
+                  begin="0.1s"
+                  dur="0.4s"
+                  values="22;18"
+                />
+                <animate
+                  fill="freeze"
+                  attributeName="cy"
+                  begin="0.1s"
+                  dur="0.4s"
+                  values="2;6"
+                />
+                <animate
+                  fill="freeze"
+                  attributeName="r"
+                  begin="0.1s"
+                  dur="0.4s"
+                  values="3;12"
+                />
+              </circle>
+              <circle
+                cx="22"
+                cy="2"
+                r="1">
+                <animate
+                  fill="freeze"
+                  attributeName="cx"
+                  begin="0.1s"
+                  dur="0.4s"
+                  values="22;18"
+                />
+                <animate
+                  fill="freeze"
+                  attributeName="cy"
+                  begin="0.1s"
+                  dur="0.4s"
+                  values="2;6"
+                />
+                <animate
+                  fill="freeze"
+                  attributeName="r"
+                  begin="0.1s"
+                  dur="0.4s"
+                  values="1;10"
+                />
+              </circle>
+            </mask>
+            <circle
+              cx="12"
+              cy="12"
+              r="6"
+              fill="currentColor"
+              mask="url(#lineMdSunnyOutlineToMoonTransition0)">
+              <set
+                attributeName="opacity"
+                begin="0.5s"
+                to="0"
+              />
+              <animate
+                fill="freeze"
+                attributeName="r"
+                begin="0.1s"
+                dur="0.4s"
+                values="6;10"
+              />
+            </circle>
+          </svg>
+          <svg
+            className={sun}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24">
+            <g
+              fill="#fff"
+              stroke="#fff"
+              stroke-dasharray="2"
+              stroke-dashoffset="2"
+              stroke-linecap="round"
+              stroke-width="2">
+              <path d="M0 0">
+                <animate
+                  fill="freeze"
+                  attributeName="d"
+                  begin="1.2s"
+                  dur="0.2s"
+                  values="M12 19v1M19 12h1M12 5v-1M5 12h-1;M12 21v1M21 12h1M12 3v-1M3 12h-1"
+                />
+                <animate
+                  fill="freeze"
+                  attributeName="stroke-dashoffset"
+                  begin="1.2s"
+                  dur="0.2s"
+                  values="2;0"
+                />
+              </path>
+              <path d="M0 0">
+                <animate
+                  fill="freeze"
+                  attributeName="d"
+                  begin="1.5s"
+                  dur="0.2s"
+                  values="M17 17l0.5 0.5M17 7l0.5 -0.5M7 7l-0.5 -0.5M7 17l-0.5 0.5;M18.5 18.5l0.5 0.5M18.5 5.5l0.5 -0.5M5.5 5.5l-0.5 -0.5M5.5 18.5l-0.5 0.5"
+                />
+                <animate
+                  fill="freeze"
+                  attributeName="stroke-dashoffset"
+                  begin="1.5s"
+                  dur="1.2s"
+                  values="2;0"
+                />
+              </path>
+            </g>
+            <g fill="#fff">
+              <path d="M15.22 6.03L17.75 4.09L14.56 4L13.5 1L12.44 4L9.25 4.09L11.78 6.03L10.87 9.09L13.5 7.28L16.13 9.09L15.22 6.03Z">
+                <animate
+                  fill="freeze"
+                  attributeName="fill-opacity"
+                  dur="0.4s"
+                  values="1;0"
+                />
+              </path>
+              <path d="M19.61 12.25L21.25 11L19.19 10.95L18.5 9L17.81 10.95L15.75 11L17.39 12.25L16.8 14.23L18.5 13.06L20.2 14.23L19.61 12.25Z">
+                <animate
+                  fill="freeze"
+                  attributeName="fill-opacity"
+                  begin="0.2s"
+                  dur="0.4s"
+                  values="1;0"
+                />
+              </path>
+            </g>
+            <g
+              fill="#fff"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2">
+              <path d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z" />
+              <set
+                attributeName="opacity"
+                begin="0.6s"
+                to="0"
+              />
+            </g>
+            <mask id="lineMdMoonToSunnyOutlineTransition0">
+              <circle
+                cx="12"
+                cy="12"
+                r="12"
+                fill="#fff"
+              />
+              <circle
+                cx="12"
+                cy="12"
+                r="8">
+                <animate
+                  fill="freeze"
+                  attributeName="r"
+                  begin="0.6s"
+                  dur="0.4s"
+                  values="8;4"
+                />
+              </circle>
+              <circle
+                cx="18"
+                cy="6"
+                r="12"
+                fill="#fff">
+                <animate
+                  fill="freeze"
+                  attributeName="cx"
+                  begin="0.6s"
+                  dur="0.4s"
+                  values="18;22"
+                />
+                <animate
+                  fill="freeze"
+                  attributeName="cy"
+                  begin="0.6s"
+                  dur="0.4s"
+                  values="6;2"
+                />
+                <animate
+                  fill="freeze"
+                  attributeName="r"
+                  begin="0.6s"
+                  dur="0.4s"
+                  values="12;3"
+                />
+              </circle>
+              <circle
+                cx="18"
+                cy="6"
+                r="10">
+                <animate
+                  fill="freeze"
+                  attributeName="cx"
+                  begin="0.6s"
+                  dur="0.4s"
+                  values="18;22"
+                />
+                <animate
+                  fill="freeze"
+                  attributeName="cy"
+                  begin="0.6s"
+                  dur="0.4s"
+                  values="6;2"
+                />
+                <animate
+                  fill="freeze"
+                  attributeName="r"
+                  begin="0.6s"
+                  dur="0.4s"
+                  values="10;1"
+                />
+              </circle>
+            </mask>
+            <circle
+              cx="12"
+              cy="12"
+              r="10"
+              fill="#fff"
+              mask="url(#lineMdMoonToSunnyOutlineTransition0)"
+              opacity="0">
+              <set
+                attributeName="opacity"
+                begin="0.6s"
+                to="1"
+              />
+              <animate
+                fill="freeze"
+                attributeName="r"
+                begin="0.6s"
+                dur="0.4s"
+                values="10;6"
+              />
+            </circle>
+          </svg>
+        </span>
+        <span className="watch">{date}</span>
+        <img
+          className="red"
+          src="/Group 1@2x.png"
+        />
+
+        <img
+          className="img2"
+          src="/Group 2.png"
+        />
+      </div>
     </div>
   );
 }
